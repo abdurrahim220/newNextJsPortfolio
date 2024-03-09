7e90276ea02d78e6504a59b114303db1c2a69b00
abdurrahim220 <z.abdurrahim5@gmail.com>
2024-03-10 00:57:35 +0600

    all done

---
+++ components/AboutSection.jsx
@@ -0,0 +1,144 @@
+"use client";
+import React, { useTransition, useState } from "react";
+import Image from "next/image";
+import TabButton from "./TabButton";
+
+const TAB_DATA = [
+  {
+    title: "Language",
+    id: "language",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>JavaScript</li>
+        <li>TypeScript</li>
+        <li>C</li>
+        <li>C++</li>
+        <li>SQL</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Education",
+    id: "education",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>
+          Bachelor of Science in Computer Science and Engineering | Southeast
+          University
+        </li>
+        <li>Full Stack Web Development | Programming Hero</li>
+      </ul>
+    ),
+  },
+  {
+    title: "FrameWorks",
+    id: "frameworks",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>React</li>
+        <li>NextJs</li>
+        <li>Redux</li>
+        <li>Node.Js</li>
+        <li>Express</li>
+        <li>MongoDB</li>
+        <li>Mongoose</li>
+        <li> Firebase</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Tools",
+    id: "tools",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>REST APIs</li> <li>VS Code</li> <li>Git </li>
+        <li>Figma</li> <li>Netlify</li> <li>Vercel</li> <li>Render</li>
+        <li>Postman</li> <li>Stripe</li> <li>JWT</li>
+        <li>Ubuntu Os</li>
+        <li>Windows OS</li>
+      </ul>
+    ),
+  },
+  // {
+  //   title: "Certifications",
+  //   id: "certifications",
+  //   content: (
+  //     <ul className="list-disc pl-2">
+  //       <li>AWS Cloud Practitioner</li>
+  //       <li>Google Professional Cloud Developer</li>
+  //     </ul>
+  //   ),
+  // },
+];
+
+const AboutSection = () => {
+  const [tab, setTab] = useState("language");
+  const [isPending, startTransition] = useTransition();
+
+  const handleTabChange = (id) => {
+    startTransition(() => {
+      setTab(id);
+    });
+  };
+
+  return (
+    <section className="text-white" id="about">
+      <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
+        <Image
+          src="/images/about-image.png"
+          width={500}
+          height={500}
+          alt="images"
+        />
+        <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
+          <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
+          <p className="text-base lg:text-lg">
+            I am a full stack web developer with a passion for creating
+            interactive and responsive web applications. I have experience
+            working with JavaScript, React, Redux, Node.js, Express,SQL,
+            Sequelize, HTML, CSS, and Git. I am a quick learner and I am always
+            looking to expand my knowledge and skill set. I am a team player and
+            I am excited to work with others to create amazing applications.
+          </p>
+          <div className="flex flex-row justify-start mt-8">
+            <TabButton
+              selectTab={() => handleTabChange("language")}
+              active={tab === "language"}
+            >
+              {" "}
+              Language{" "}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("frameworks")}
+              active={tab === "frameworks"}
+            >
+              {" "}
+              Frameworks{" "}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("tools")}
+              active={tab === "tools"}
+            >
+              {" "}
+              Tools{" "}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("education")}
+              active={tab === "education"}
+            >
+              {" "}
+              Education{" "}
+            </TabButton>
+          </div>
+          <div className="mt-8">
+            {TAB_DATA.find((t) => t.id === tab).content}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default AboutSection;
---
+++ components/ProjectsSection.jsx
@@ -0,0 +1,170 @@
+"use client";
+import React, { useState, useRef } from "react";
+import ProjectCard from "./ProjectCard";
+import ProjectTag from "./ProjectTag";
+import { motion, useInView } from "framer-motion";
+
+const projectsData = [
+  {
+    id: 1,
+    title: "RahimStore",
+    description: "e-commerce website store",
+    image: "https://i.ibb.co/qg73Lhg/rahim-Store.png",
+    tag: ["FullStack", ""],
+    gitUrl: "https://github.com/abdurrahim220/rahim_store.git",
+    liveLink: "https://rahimstore-67b51.web.app",
+  },
+  {
+    id: 2,
+    title: "MovieX",
+    description: "Movie site like netflix",
+    image: "https://i.ibb.co/3NSX9Hm/moviex-dun-vercel-app.png",
+    tag: ["Frontend", ""],
+    gitUrl: "https://github.com/abdurrahim220/moviex",
+    liveLink: "https://moviex-dun.vercel.app",
+  },
+  {
+    id: 3,
+    title: "DhakaBusTicket",
+    description: "User can bus ticket from here",
+    image: "https://i.ibb.co/qsKZ0G4/dhaka-bus-ticket.png",
+    tag: ["FullStack", ""],
+    gitUrl: "https://github.com/abdurrahim220/BusTicket",
+    liveLink: "https://658b9bc256d7341880d1a1bb--grand-manatee-e3657d.netlify.app/",
+  },
+  {
+    id: 4,
+    title: "Toy Store",
+    description: "It's a toy store site",
+    image:
+      "https://i.ibb.co/hdD7pNB/screencapture-cat-toy-e6d24-web-app-2023-12-25-22-28-39.png",
+    tag: ["FullStack", ""],
+    gitUrl: "https://github.com/abdurrahim220/ToyStore_Server.git",
+    liveLink: "https://cat-toy-e6d24.web.app/",
+  },
+  {
+    id: 5,
+    title: "CarerHub",
+    description: "This is a job finder web site",
+    image:
+      "https://i.ibb.co/ZKtB4p7/screencapture-find-job-omega-vercel-app-2023-12-26-12-03-10.png",
+    tag: ["Frontend", ""],
+    gitUrl: "https://github.com/abdurrahim220/FindJob.git",
+    liveLink: "https://find-job-omega.vercel.app",
+  },
+  {
+    id: 6,
+    title: "Influencer Hub",
+    description: "This is a influencer hub landing page",
+    image:
+      "https://i.ibb.co/9hrWfBg/screencapture-influencer-cyan-vercel-app-2023-12-26-12-16-44.png",
+    tag: ["Frontend", ""],
+    gitUrl: "https://github.com/abdurrahim220/influencer.git",
+    liveLink: "https://influencer-cyan.vercel.app",
+  },
+  {
+    id: 7,
+    title: "Convention",
+    description: "This is a Convention hub landing page",
+    image:
+      "https://i.ibb.co/PMmBvVK/screencapture-convention-seven-vercel-app-2023-12-26-12-27-51.png",
+    tag: ["Frontend", ""],
+    gitUrl: "https://github.com/abdurrahim220/convention.git",
+    liveLink: "https://convention-seven.vercel.app",
+  },
+  {
+    id: 8,
+    title: "Full-stack Roadmap",
+    description: "Project 5 description",
+    image: "/images/projects/6.png",
+    tag: ["All", "Web"],
+    gitUrl: "/",
+    liveLink: "/",
+  },
+  {
+    id: 9,
+    title: "House Hunting",
+    description: "Project 5 description",
+    image:
+      "https://i.ibb.co/HdCSH3k/screencapture-gentle-sprinkles-7fc787-netlify-app-2024-01-29-18-44-03-min.png",
+    tag: ["FullStack", ""],
+    gitUrl: "https://github.com/abdurrahim220/houseRent_Backend.git",
+    liveLink: "https://gentle-sprinkles-7fc787.netlify.app/",
+  },
+  {
+    id: 10,
+    title: "Image Ai",
+    description: "Edit or update your image with the help of Ai",
+    image:
+      "https://i.ibb.co/N7hm4mq/imageai.png",
+    tag: ["NextJs", ""],
+    gitUrl: "https://github.com/abdurrahim220/image",
+    liveLink: "https://rahim-imageai.vercel.app/",
+  },
+];
+
+const ProjectsSection = () => {
+  const [tag, setTag] = useState("FullStack");
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true });
+
+  const handleTagChange = (newTag) => {
+    setTag(newTag);
+  };
+
+  const filteredProjects = projectsData.filter((project) =>
+    project.tag.includes(tag)
+  );
+
+  const cardVariants = {
+    initial: { y: 50, opacity: 0 },
+    animate: { y: 0, opacity: 1 },
+  };
+
+  return (
+    <section id="projects">
+      <h2 className="text-center text-4xl font-bold text-white mt-4 mb-8 md:mb-12">
+        My Projects
+      </h2>
+      <div className="text-white flex flex-row justify-center items-center gap-2 py-6">
+        <ProjectTag
+          onClick={handleTagChange}
+          name="FullStack"
+          isSelected={tag === "FullStack"}
+        />
+        <ProjectTag
+          onClick={handleTagChange}
+          name="NextJs"
+          isSelected={tag === "NextJs"}
+        />
+        <ProjectTag
+          onClick={handleTagChange}
+          name="Frontend"
+          isSelected={tag === "Frontend"}
+        />
+      </div>
+      <ul ref={ref} className="grid md:grid-cols-3 gap-8 md:gap-12">
+        {filteredProjects.map((project, index) => (
+          <motion.li
+            key={index}
+            variants={cardVariants}
+            initial="initial"
+            animate={isInView ? "animate" : "initial"}
+            transition={{ duration: 0.3, delay: index * 0.4 }}
+          >
+            <ProjectCard
+              key={project.id}
+              title={project.title}
+              description={project.description}
+              imgUrl={project.image}
+              gitUrl={project.gitUrl}
+              previewUrl={project.liveLink}
+            />
+          </motion.li>
+        ))}
+      </ul>
+    </section>
+  );
+};
+
+export default ProjectsSection;
